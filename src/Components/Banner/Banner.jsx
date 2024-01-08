34931b90575da1c3f6bf160a973276329039ea49
unknown <jonathanpoblet228@gmail.com>
2024-01-08 17:03:25 -0300

    deploy

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,11 +5,7 @@ export default function Banner() {
   return (
     <section className='banner'>
       <article className='banner-container'>
-        <img
-          className='banner-container-forza'
-          src='/assets/banner/forza.png'
-          alt='forza'
-        />
+        <img className='banner-container-forza' src='assets/banner/forza.png' alt='forza' />
         <h1 className='banner-container-title'>
           <span>Forza</span>GYM
         </h1>
